cb5c69a3d15d53b5b1046775daf953acf5918537
Ammulyk1 <likhithaneligonda@gmail.com>
2023-09-04 17:11:19 +0530

    update userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,17 @@
 const router = require('express').Router();
 const User = require('../modules/User');
 
+router.get('/members', async (req, res) => {
+  try {
+    // Query the database to get members
+    const members = await User.find(); // Assuming User is your model
+    res.json(members);
+  } catch (error) {
+    console.error('Error fetching members:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 //creating user
 router.post('/', async(req, res)=> {
     try {
